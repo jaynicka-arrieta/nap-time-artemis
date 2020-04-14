74a441499ecf9690c9f400ac2dc023a0259a2db2
elijahbaird <ebaird@csumb.edu>
2020-04-13 23:43:49 -0700

    Added EndTrigger.cs which plays the end conversation when the invisible end point is reached, and changed how the speed is set in Movement.cs to be controlled by Fungus

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed;
+    public Flowchart flowchart;
+
     // Start is called before the first frame update
     void Start()
     {
+        flowchart = GameObject.FindObjectOfType<Flowchart>();
+        speed = 0;
         Debug.Log($"start");
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = flowchart.GetFloatVariable("currentSpeed");
         float diff = 0;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
