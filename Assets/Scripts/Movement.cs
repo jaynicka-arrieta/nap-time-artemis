dbc90a779c1848eef6ff3df7abbb44e5f33e3e88
Jaynicka <jaynicka8@yahoo.com>
2020-04-14 01:19:11 -0700

    Added jump and camera follow

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,30 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
+public class Movement : MonoBehaviour {
+
+    [SerializeField] private LayerMask platformLayerMask;
+    private Rigidbody2D rigidBody;
+    private Collider2D collider;
     public float speed = 1;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Debug.Log($"start");
+        rigidBody = transform.GetComponent<Rigidbody2D>();
+        collider = transform.GetComponent<Collider2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float diff = 0;
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             diff -= speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             diff += speed * Time.deltaTime;
         }
 
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+            float jumpVelocity = 10f;
+            rigidBody.velocity = Vector2.up * jumpVelocity;
+        }
+
         transform.Translate(new Vector3(diff, 0, 0));
-    
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, 0.01f, platformLayerMask);
+        return (raycastHit.collider != null);
     }
 }
