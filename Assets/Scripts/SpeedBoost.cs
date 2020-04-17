fc73fc50ecc6c6d68320799e7cd165f6b1ecd568
elijahbaird <ebaird@csumb.edu>
2020-04-16 17:57:43 -0700

    added fungus stuff to stage 1 and created and implemented speed boost with timers

---
+++ Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public Flowchart flowchart;
+    void OnTriggerEnter2D (Collider2D other) {
+        flowchart = GameObject.FindObjectOfType<Flowchart>();
+        flowchart.ExecuteBlock("SpeedBoost");
+    }
+}
