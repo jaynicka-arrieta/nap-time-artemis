52d8cfcee4b5ff1ec47676bf0e4655e08a5863b3
elijahbaird <ebaird@csumb.edu>
2020-04-21 15:59:09 -0700

    Added fungus to all three levels and edited it to cover some edge cases

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using Fungus;
 
 public class EndTrigger : MonoBehaviour
 {
     public Flowchart flowchart;
+
+     void Start() {
+        flowchart = GameObject.FindObjectOfType<Flowchart>();
+    }
+    
     void OnTriggerEnter2D (Collider2D other) {
         flowchart.ExecuteBlock("End");
     }
