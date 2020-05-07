8ed68d5ca055359fd611ad0d67f285cc74e7554e
elijahbaird <ebaird@csumb.edu>
2020-05-07 15:08:13 -0700

    Added dog enemies, attached their animation, added death mechanic to dogs, and changed respawn from script to fungus

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Death : MonoBehaviour
+{
+ public Flowchart flowchart;
+
+     void Start() {
+        flowchart = GameObject.FindObjectOfType<Flowchart>();
+    }
+    
+    void OnTriggerEnter2D (Collider2D other) {
+        Debug.Log("dead");
+        flowchart.ExecuteBlock("Death");
+    }
+}
