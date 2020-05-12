789f5fca4dbcf67e39dd7d157bf18643e3664bfe
Jaynicka <jaynicka8@yahoo.com>
2020-05-12 03:13:39 -0700

    finished

---
+++ Assets/Scripts/nextScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class nextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VideoPlayer video;
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!video.isPlaying) {
+            SceneManager.LoadScene(0);
+            Debug.Log("reached end of video");
+        }
+    }
+
+
+
+
+}
